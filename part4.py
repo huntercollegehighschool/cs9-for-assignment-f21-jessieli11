9273a361d85f3a3483cdb4ac22b1b4673e34b75d
nonname001 <austin2004@gmail.com>
2022-01-15 19:04:12 -0500

    added solution for part 4

--- part4.py
+++ part4.py
@@ -40,3 +40,15 @@ fizz
 """
 
 # write your code here 
+
+n = int(input("Enter a number: "))
+
+for i in range(n+1):
+  if i % 15 == 0:
+    print("fizzbuzz")
+  elif i % 3 == 0:
+    print("fizz")
+  elif i % 5 == 0:
+    print("buzz")
+  else:
+    print(i)
